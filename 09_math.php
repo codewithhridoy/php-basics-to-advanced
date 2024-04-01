619fd990cea7001cc4fb29a9b9a5cb815ea1b1a0
hridoy.dev <codewithhridoy@gmail.com>
2024-04-01 23:35:24 +0600

    feat: math functions

---
+++ 09_math.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * All Math functions: https://www.w3schools.com/php/php_ref_math.asp
+ * */
+
+// pi() Function
+echo "<pre/>======== pi() ======== <pre/>";
+echo(pi());
+
+// min() Function
+echo "<pre/>======== min() ======== <pre/>";
+echo(min(0, 150, 30, 20, -8, -200));
+
+// max() Function
+echo "<pre/>======== max() ======== <pre/>";
+echo(max(0, 150, 30, 20, -8, -200));
+
+// abs() Function
+echo "<pre/>======== abs() ======== <pre/>";
+echo(abs(-5.69));
+
+// sqrt() Function
+echo "<pre/>======== sqrt() ======== <pre/>";
+echo(sqrt(16));
+
+// round() Function
+echo "<pre/>======== round() ======== <pre/>";
+echo(round(0.50));
+echo "<pre/>";
+echo(round(0.49));
+
+// rand() Function
+echo "<pre/>======== rand() ======== <pre/>";
+echo(rand());
+
+// rand() range Function
+echo "<pre/>======== rand() range ======== <pre/>";
+echo(rand(10, 100));
